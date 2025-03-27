2e6c560773b001c66a86e3b9a5b260fa5cfda98c
Aritree Hoque <aritree.hoque@gmail.com>
2025-03-27 03:28:34 -0400

    Added comments throughout

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,25 +1,25 @@
 // Task2- Fetch Products with .then()
 function fetchProductsThen() {
-    fetch('https://www.course-api.com/javascript-store-products')
-    .then(response => response.json())
+    fetch('https://www.course-api.com/javascript-store-products') // Getting product data from API
+    .then(response => response.json()) // Turniing response into data
     .then(products => {
         products.forEach(product => {
-            console.log(product.fields.name);
+            console.log(product.fields.name); // Showing each product name in console
         });
     })
     .catch(error => {
-        console.log('Error fetching products', error);
+        console.log('Error fetching products', error); // Showing error if fetch fails
     });
 }
 
 // Task3- Fetch Products with async/await
 async function fetchProductsAsync() {
-    try {
+    try { // To get and show products
         const response = await fetch('https://www.course-api.com/javascript-store-products');
         const products = await response.json();
-        displayProducts(products);
-    } catch (error) {
-        handleError(error);
+        displayProducts(products); // Displaying products on the page
+    } catch (error) { // Catching errors
+        handleError(error); // Hnadling errors
     }
 }
 
@@ -27,24 +27,24 @@ async function fetchProductsAsync() {
 function displayProducts(products) {
     const container = document.getElementById('product-container');
     container.innerHTML = '';
-    products.slice(0, 5).forEach(product => {
-        const fields = product.fields;
+    products.slice(0, 5).forEach(product => { // Showing first 5 products
+        const fields = product.fields; // Getting product details
         const productElement = document.createElement('div');
-        productElement.className = 'product';
+        productElement.className = 'product'; // Adding new product class
         productElement.innerHTML = `
         <img src="${fields.image[0].url}" alt="${fields.name}" width="200">
         <h2>${fields.name}</h2>
         <p>Price: $${fields.price / 100}</p>
-        `;
-        container.appendChild(productElement);
+        `; // Adding image, name, and price to product
+        container.appendChild(productElement); // Appending to the contianer
     })
 }
 
 // Task5- Reusable Error Handler
 function handleError(error) {
-    console.log("An error occurred: ", error.message);
+    console.log("An error occurred: ", error.message); // Logging error message in console
 }
 
 // Task6- Call Your Fetch Functions
 fetchProductsThen();
-fetchProductsAsync();
+fetchProductsAsync(); // Running fetch functions
